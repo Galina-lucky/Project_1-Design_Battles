ae0a909049ac3cb0270b240c5db823bb48995c40
Galina-lucky <gala.nitch@gmail.com>
2021-02-02 00:59:10 +0200

    Tuned gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,154 @@
-// Определяем константы Gulp
-const { src, dest, parallel, series, watch } = require('gulp');
-const browserSync = require('browser-sync').create();
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
+"use strict";
+
+const gulp = require('gulp');
+const notify = require("gulp-notify");
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const cleancss = require('gulp-clean-css');
+const csso = require('gulp-csso');
+const terser = require('gulp-terser');
+const rename = require("gulp-rename");
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
+const plumber = require('gulp-plumber');
+const argv = require('yargs').argv;
+const concat = require('gulp-concat');
+const through2 = require('through2');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
 const del = require('del');
 
-function html() {
-  return src('src/**/*.html') 
-  .pipe(dest('dist/'))
-  .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
+let isSound = false;
+
+
+/*== Paths to source/build/watch files ==*/
+let path = {
+    build: {
+        js: 'dist/js/',
+        css: 'dist/css/',
+        html: 'dist/',
+        img: 'dist/img/'
+    },
+    src: {
+        js: 'src/js/',
+        css: 'src/scss/main.scss',
+        html: 'src/*.html',
+        img: 'src/img/**/*'
+    },
+    watch: {
+        js: "src/js/",
+        css: 'src/scss/**/*.scss',
+        html: 'src/*.html',
+        img: 'src/img/**/*'
+    }
+};
+
+/* Compile SASS */
+const styles = (done) => {
+    gulp.src(path.src.css)
+        .pipe(plumber())
+        .pipe(gulpIf(argv.dev, sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(csso())
+        .pipe(gulpIf(argv.dev, sourcemaps.write()))
+        .pipe(gulp.dest(path.build.css))
+        .pipe(gulpIf(argv.dev, notify({message: 'Styles task complete', onLast: true, sound: isSound})))
+        .pipe(gulpIf(argv.dev, browserSync.stream()));
+    done();
 }
-
-function styles() {
-  return src('src/scss/main.scss')
-  .pipe(sass().on('error', sass.logError)) 
-  .pipe(concat('main.min.css')) // Конкатенируем в файл app.min.js
-  .pipe(autoprefixer({ overrideBrowserslist: ['last 3 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
-  .pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
-  .pipe(dest('dist/css/')) // Выгрузим результат в папку "src/css/"
-  .pipe(browserSync.stream()) // Сделаем инъекцию в браузер
-}
-
-function scripts() {
-  return src([ // Берём файлы из источников
-    'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
-    'src/js/**/*.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
-    ])
-  .pipe(concat('main.min.js')) // Конкатенируем в один файл
-  .pipe(uglify()) // Сжимаем JavaScript
-  .pipe(dest('dist/js/')) // Выгружаем готовый файл в папку назначения
-  .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
-}
-
-function images() {
-  return src('src/img/**/*') // Берём все изображения из папки источника
-  .pipe(newer('dist/img/')) // Проверяем, было ли изменено (сжато) изображение ранее
-  .pipe(imagemin()) // Сжимаем и оптимизируем изображеня
-  .pipe(dest('dist/img/')) // Выгружаем оптимизированные изображения в папку назначения
-}
-
-function cleanimg() {
-  return del('dist/img/**/*', { force: true }) // Удаляем всё содержимое папки "src/images/dest/"
-}
-
-function cleandist() {
-  return del('dist/**/*', { force: true }) // Удаляем всё содержимое папки "dist/"
-}
-
-function startwatch() { 
-  // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
-  watch(['src/js/**/*.js', '!src/**/*.min.js'], scripts);
-  
-  // Мониторим файлы препроцессора на изменения
-  watch('src/scss/**/*.scss', styles);
-
-  // Мониторим папку-источник изображений и выполняем images(), если есть изменения
-  watch('src/img/**/*', images);
-
-  // Мониторим файлы HTML на изменения
-  watch('src/**/*.html', html);
-}
-
-// Определяем логику работы Browsersync
-function browsersync() {
-  browserSync.init({ // Инициализация Browsersync
-    server: { baseDir: 'dist/' }, // Указываем папку сервера
-    notify: false, // Отключаем уведомления
-    online: true // Режим работы: true или false
-  })
-}
-
-exports.html = html;
-  // Экспортируем функцию scripts() в таск scripts
-exports.scripts = scripts;
-  // Экспортируем функцию styles() в таск styles
 exports.styles = styles;
-// Экспорт функции images() в таск images
-exports.images = images;
-// Экспортируем функцию cleanimg() как таск cleanimg
-exports.cleanimg = cleanimg;
-// Экспортируем функцию cleandist() как таск cleandist
-exports.cleandist = cleandist;
- // Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
-exports.browsersync = browsersync;
 
-// Экспортируем дефолтный таск с нужным набором функций
-exports.default = series(cleandist, html, styles, scripts, images, parallel(startwatch, browsersync));
+/* Compile JS */
+function buildJs(sourceFile, destPath, concatenationName) {
+    gulp.src(sourceFile)
+        .pipe(plumber())
+        .pipe(gulpIf(argv.dev, sourcemaps.init()))
+        .pipe(concatenationName ? concat(concatenationName) : through2.obj())
+        .pipe(gulp.dest(destPath))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(terser())
+        .pipe(gulpIf(argv.dev, sourcemaps.write('./')))
+        .pipe(gulp.dest(destPath))
+        .pipe(gulpIf(argv.dev, notify({ message: (concatenationName ? concatenationName : 'Scripts Single') + ' task complete', onLast: true, sound: isSound })))
+        .pipe(gulpIf(argv.dev, browserSync.stream()));
+}
+
+let listSrcMain = [
+    'node_modules/jquery/dist/jquery.min.js',
+    path.src.js +'**/*.js',
+]
+
+
+/* Build Main JS */
+const scriptsMain = (done) => {
+    buildJs(listSrcMain, path.build.js, 'main.js');
+    done();
+}
+exports.scriptsMain = scriptsMain;
+
+
+/* Task HTML */
+const html = (done) => {
+  gulp.src(path.src.html)
+      .pipe(gulp.dest(path.build.html))
+      .pipe(gulpIf(argv.dev, notify({message: 'HTML task complete', onLast: true, sound: isSound})))
+      .pipe(gulpIf(argv.dev, browserSync.stream()));
+  done();
+}
+exports.html = html;
+
+
+/*Optimize Images*/
+const images = (done) => {
+  gulp.src(path.src.img)
+      .pipe(plumber())
+      .pipe(newer(path.build.img))
+      .pipe(imagemin()) //не настроен
+      .pipe(gulp.dest(path.build.img))
+      .pipe(gulpIf(argv.dev, browserSync.stream()));
+  done();    
+}
+exports.images = images;
+
+const cleanimg = () => {
+  return del('dist/img/**/*', { force: true });
+}
+exports.cleanimg = cleanimg;
+
+const cleandist = () => {
+  return del('dist/**/*', { force: true });
+}
+exports.cleandist = cleandist;
+
+
+/* Browser-sync Static server */
+const browserSyncServer = () => {
+    browserSync.init({
+        server: { baseDir: 'dist/' },
+        notify: false, // что это?
+        online: true // что это?
+    });
+}
+exports.browserSyncServer = browserSyncServer;
+
+
+/* Watcher */
+const watch = () => {
+    gulp.watch(path.watch.css, styles);
+    gulp.watch(path.watch.js + '*.js', scriptsMain);
+    gulp.watch(path.watch.img, images);
+    gulp.watch(path.watch.html, html);
+}
+exports.watch = watch;
+
+
+/* Production build Task */
+const prod = gulp.series(cleandist, images, html, styles, scriptsMain);
+exports.prod = prod;
+
+/* Develop build Task */
+const dev = gulp.series(prod, gulp.parallel(watch, browserSyncServer));
+exports.default = dev;
